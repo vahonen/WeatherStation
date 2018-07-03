68c18b43eee13a808e0f2aa222fdad872a18a2d1
vahonen <31847397+vahonen@users.noreply.github.com>
2018-07-03 19:48:08 +0300

    Add files via upload

--- weather_station_DHT_TS.py
+++ weather_station_DHT_TS.py
@@ -4,64 +4,21 @@
 import Adafruit_DHT as dht
 import time
 import os
-import MySQLdb
 import urllib2
 
-def connect_database():
-    con = MySQLdb.connect(host = "192.168.0.112", user = "testaaja", passwd = "salasana", db = "dht")
-    return con
-
-def read_parameter(curs, parameter_name):
-    sql_query = "SELECT value FROM parameters WHERE name = %s"
-    
-    success = False
-    value = "0"
-
-    try:      
-        curs.execute (sql_query, (parameter_name))
-        results = curs.fetchall()
-        for row in results:
-            value = row[0]
-        success = True
-    except:
-        print( "Virhe: parametria ei voitu lukea")
-
-    return success, value
 
 def main():
     
-    DEFAULT_INTERVAL = 900 #oletuksena 15 minuutin periodi
     GPIO_PIN = 23
-
-    enabled = 1
+    DEFAULT_INTERVAL = 10 # 10 sekuntia
     
-    interval = DEFAULT_INTERVAL
+    enabled = 1
+  
     f = open("api_key","r")
-    API_KEY = f.readline()[:-1]
+    WRITE_KEY = f.readline()[:-1]
     f.close()
     
     while True:
-        
-        con = connect_database();
-        curs=con.cursor()
-        
-        success, enabled_ = read_parameter(curs, "enabled")
-        if success:
-            enabled = int(enabled_)
-        else:
-            enabled = 1
-            print( "Virhe: parametrin luku ei onnistunut (2)")
-
-        print("ulompi looppi: enabled %s" % (enabled)) #testitulostusta
-        if enabled == 1:
-            success, interval_ = read_parameter(curs, "interval")
-            if success:
-                interval = int(interval_)
-            else:
-                interval = DEFAULT_INTERVAL
-                print( "Virhe: parametrin luku ei onnistunut (3)")
-        print("ulompi: interval %s" % (interval)) #testitulostusta
-        
         while enabled == 1:
             humidity, temperature = dht.read_retry(dht.DHT22, GPIO_PIN)
             temperature = round(temperature, 1)
@@ -81,68 +38,18 @@ def main():
             print("temperature %s" % temperature)
             print("humidity %s" % humidity)
 
-            # muodostetaan yhteys LAMP-serverin tietokantaan
-            con = connect_database()
-            curs = con.cursor()
-
-            sql_query = "INSERT INTO data_table (date, time, temperature, humidity) VALUES (%s, %s, %s, %s)"
-            
-             # tietokantaan talletus
-            try:
-                curs.execute(sql_query, (date_, time_, temperature, humidity))
-                con.commit()
-                print("Data talletettu tietokantaan")
-
-            except:
-                print( "Virhe: dataa ei talletettu tietokantaan")
-                con.rollback()
-
-             # ThingSpeakiin vienti
+           
+           
+            # ThingSpeakiin vienti
             try:
         
-                f = urllib2.urlopen("https://api.thingspeak.com/update?api_key=%s&field1=%s&field2=%s" % (API_KEY, temperature, humidity))
+                f = urllib2.urlopen("https://api.thingspeak.com/update?api_key=%s&field1=%s&field2=%s" % (WRITE_KEY, temperature, humidity))
                 f.close()
                 print("Data lähetetty ThingSpeakiin")
 
             except:
                 print( "Virhe avattaessa url:ia")
 
-            # luetaan parametrit (mittausjakso: "interval", onko mittaus päällä: "enabled")
-            success, interval_ = read_parameter(curs, "interval")
-            if success:
-                interval = int(interval_)
-            else:
-                interval = 900
-            
-            success, enabled_ = read_parameter(curs, "enabled")
-            if success:
-                enabled = int(enabled_)
-            else:
-                enabled = 1
-
-            print("success: %s interval: %s" % (success, interval))
-            print("success: %s enabled: %s" % (success, enabled))
-        
-            counter = 0
-            read_counter = 0
-            while counter < interval and enabled == 1:
-                time.sleep(1)
-                counter += 1
-                read_counter += 1
-
-                if read_counter > 10:
-                    con = connect_database()
-                    curs=con.cursor()
-                    success, enabled_ = read_parameter(curs, "enabled")
-                    if success:
-                        enabled = int(enabled_)
-                    else:
-                        enabled = 1
-                        print( "Virhe: parametrin luku ei onnistunut (1)")
-                    read_counter = 0
-                    print("sisempi looppi: enabled %s" % (enabled)) #testitulostust
-        
-        time.sleep(10) #odotetaan 10 s ennen kuin seuraavan kerran tarkistetaan onko mittaus päällä
-
+            time.sleep(DEFAULT_INTERVAL)
 if __name__ == "__main__":
     main()
